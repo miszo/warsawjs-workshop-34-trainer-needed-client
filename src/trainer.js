a5b96b69279dec485ca949d877e69bd242ba4c2f
Jacek Mikrut <jacekmikrut@gmail.com>
2019-07-27 15:32:46 +0200

    [STEP 12] As a trainer wait for help requests.

--- src/trainer.js
+++ src/trainer.js
@@ -6,8 +6,8 @@ export function connectAsTrainer() {
   data.webSocket = new WebSocket(`ws://localhost:${WEB_SOCKET_PORT}`);
 
   data.webSocket.addEventListener('open', function() {
-    ui.showMessage('Connected!');
     data.webSocket.send(JSON.stringify({ type: 'identification', role: data.role, identification: data.identification }));
+    ui.showMessage('Waiting for help requests...');
   });
 
   data.webSocket.addEventListener('close', function() {
